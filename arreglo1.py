0eb38c8ffaaf8fce89a136705e326115d360497c
Perlita <160614177+PEPERJHGFD@users.noreply.github.com>
2024-10-07 15:46:42 +0000

    Agregar código de ejemplo

---
+++ arreglo1.py
@@ -0,0 +1,11 @@
+ Definimos un arreglo unidimensional
+arreglo = [10, 20, 30, 40, 50]
+
+# Inicializamos la suma en 0
+suma = 0
+
+# Recorremos el arreglo y sumamos los elementos
+for numero in arreglo:
+    suma += numero
+
+print("La suma de los elementos es:", suma)
